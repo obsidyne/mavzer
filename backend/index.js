4e0e230a316e490ca412c24c040cba953a0f1932
navadeep satheesh <navadeepsatheesh@gmail.com>
2026-04-04 19:18:32 +0530

    addd 0.0.0.0

--- backend/index.js
+++ backend/index.js
@@ -42,4 +42,4 @@ app.use("/api/products",   auth, productRoutes);
 app.use("/api/upload",     auth, uploadRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0" , () => console.log(`Server running on port ${PORT}`));
